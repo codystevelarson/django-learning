04d9d4e0b96cb8581a84202ab2aa8670a5be8262
Cody Larson <cody.larson@fjorgedigital.com>
2020-09-08 11:48:09 -0500

    Started forms project

---
+++ Django4/emilyspizza-project/pizza/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .forms import PizzaForm
+# Create your views here.
+
+def home(request):
+    return render(request, 'pizza/home.html')
+
+
+def order(request):
+    if request.method == 'POST':
+        filled_form = PizzaForm(request.POST)
+        if filled_form.is_valid():
+            note = 'Thank you for ordering! Your %s pizza with %s and %s  is on the way' %(
+            filled_form.cleaned_data['size'],
+            filled_form.cleaned_data['topping1'],
+            filled_form.cleaned_data['topping2']
+            )
+            new_form = PizzaForm()
+            return render(request, 'pizza/order.html', {'pizzaform': new_form, 'note': note})
+    else:
+        form = PizzaForm()
+        return render(request, 'pizza/order.html', {'pizzaform': form})
+
